cf6c566a8db0e854877f694ad63fc6176f5546db
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-05-03 12:39:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "tstack.h"
 
-int priority (char ch) {
+int priority(char ch) {
   switch (ch) {
     case '(':
       return 0;
@@ -21,18 +21,16 @@ int priority (char ch) {
 
 std::string infx2pstfx(std::string inf) {
   TStack<char> stack1;
-  
   std::string  outStr;
-  
   for (int i = 0; i < inf.size(); i++) {
     if ('0' <= inf[i] && inf[i] <= '9') {
       outStr += inf[i];
       if (inf[i+1] < '0' || inf[i+1] > '9')
         outStr += " ";
     } else {
-      if ((stack1.isEmpty() || priority(inf[i]) == 0 || priority(inf[i]) > priority(stack1.get())) && priority(inf[i]) != 1)
+      if ((stack1.isEmpty() || priority(inf[i]) == 0 || priority(inf[i]) > priority(stack1.get())) && priority(inf[i]) != 1) {
         stack1.push(inf[i]);
-      else if (priority(inf[i]) != 1 && priority(inf[i]) <= priority(stack1.get())) {
+      } else if (priority(inf[i]) != 1 && priority(inf[i]) <= priority(stack1.get())) {
         while (priority(inf[i]) <= priority(stack1.get()) && priority(stack1.get()) > 0) {
           outStr += stack1.get();
           outStr += " ";
@@ -49,17 +47,14 @@ std::string infx2pstfx(std::string inf) {
       }
     }
   }
-  
   while (!stack1.isEmpty()) {
     outStr += stack1.get();
     outStr += " ";
     stack1.pop();
-  }
-  
+  }  
   return outStr;
 }
-
-int calculate (char *op, int num1, int num2) {
+int calculate(char *op, int num1, int num2) {
   switch (*op) {
     case '+':
       return num1 + num2;
